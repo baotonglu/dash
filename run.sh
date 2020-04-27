3bcc8fda04387a3e22c653250613a0565609d514
baotonglu <lubaotong22@gmail.com>
2020-04-27 08:40:37 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+# number of threads to run
 thread_num=(0 1 2 4 8 16 24 48)
-#benckmark workload
+# benckmark workload, number of opeartions to run
 workload=(0 190000000)
-#warm-up workload
+# warm-up workload, number of key-value to insert for warm-up
 base=(0 10000000)
+# type of keys
 key_type=(0 fixed variab1e)
+# which index to evaluate
 index_type=(0 dash-ex dash-lh cceh level)
+# whether to use to epcoh manager
 epoch=(0 1 1 1 0)
 
-#delete the corresponding file
-
-#{1..6}
-# k specify the testing index
+# k specify the testing index, 1 = dash-ex, 2 = dash-lh, 3 = cceh, 4 = level
 # i specify the key type, 1 means fixed-length key, 2 means variable-length key
-# j spec1fy the number of threads
-#for q in 0.5 0.6 0.7 0.8 0.9 0.99
-#do
+# j spec1fy the number of threads, 1 means one threads, 2 means two threads, 3 means four threads...
 for k in 1
 do
 	for i in 1 2
